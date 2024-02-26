a8590b259a4032cbdc408764bce3d43c3db62ae2
TheForis <boriskrstovski@gmail.com>
2024-02-27 00:54:32 +0100

    test

---
+++ C#-Basic/C#-Basic-Homeworks/1stClassHomework/1stClassHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1stClassHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      *");
+            Console.WriteLine("      *");
+            Console.WriteLine("    * * *");
+            Console.WriteLine("  *   *   *");
+            Console.WriteLine("      *");
+            Console.WriteLine("      *");
+            Console.WriteLine("    *   *");
+            Console.WriteLine("  *       *");
+
+            Console.WriteLine("~~~~~~~~~~~~~");
+
+            Console.WriteLine("      *");
+            Console.WriteLine("  *   *   *");
+            Console.WriteLine("    * * *");
+            Console.WriteLine("      *");
+            Console.WriteLine("      *");
+            Console.WriteLine("      *");
+            Console.WriteLine("    *   *");
+            Console.WriteLine("  *       *");
+        }
+    }
+}
