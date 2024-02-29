7dee79c36c952943d9276828d3141bc12449a962
TheForis <boriskrstovski@gmail.com>
2024-02-29 14:01:49 +0100

    task1-2ndClass

---
+++ C#-Basic/C#-Basic-Homeworks/2ndClassHomework/Task1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int secondNumber = 15;
+            int thirdNumber = 20;
+            int fourthNumber = 30;
+            int avgResult = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+            Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber}, {fourthNumber} is {avgResult}!");
+        }
+    }
+}
