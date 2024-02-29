052b35d1c6aff10e6fa8d45bed4065357dd21786
TheForis <boriskrstovski@gmail.com>
2024-02-29 14:35:22 +0100

    second task

---
+++ C#-Basic/C#-Basic-Homeworks/2ndClassHomework/Task2/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            string firstInput = Console.ReadLine();
+
+            int firstParsedInput;
+            bool firstParse = int.TryParse(firstInput, out firstParsedInput);
+            if (!firstParse)
+            {
+
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+
+            Console.WriteLine("Please enter the second number");
+            string secondInput = Console.ReadLine();
+
+            int secondParsedInput;
+            bool secondParse = int.TryParse(secondInput, out secondParsedInput);
+            if (!secondParse)
+            {
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+
+            Console.WriteLine($"First user input: {firstParsedInput}, second user input:{secondParsedInput}");
+
+            int firstValueHolder = firstParsedInput;
+            firstParsedInput = secondParsedInput;
+            secondParsedInput = firstValueHolder;
+
+            Console.WriteLine($"SWAPPED~~ First value = {firstParsedInput}, Second value = {secondParsedInput}");
+        }
+    }
+}
