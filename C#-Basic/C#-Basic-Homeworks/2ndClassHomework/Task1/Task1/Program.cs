b341790e2665662b047068716f7a18c09657548c
TheForis <boriskrstovski@gmail.com>
2024-03-01 10:50:20 +0100

    re

--- C#-Basic/C#-Basic-Homeworks/2ndClassHomework/Task1/Task1/Program.cs
+++ C#-Basic/C#-Basic-Homeworks/2ndClassHomework/Task1/Task1/Program.cs
@@ -4,10 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int firstNumber = 10;
-            int secondNumber = 15;
-            int thirdNumber = 20;
-            int fourthNumber = 30;
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            int fourthNumber;
+
+
+            Console.WriteLine("Please enter the first number");
+            string firstInput = Console.ReadLine();
+            bool first = int.TryParse(firstInput, out firstNumber);
+
+            Console.WriteLine("Please enter the second number");
+            string secondInput = Console.ReadLine();
+            bool second = int.TryParse(secondInput, out secondNumber);
+
+            Console.WriteLine("Please enter the third number");
+            string thirdInput = Console.ReadLine();
+            bool third = int.TryParse(thirdInput, out thirdNumber);
+
+            Console.WriteLine("Please enter the fourth number");
+            string fourthInput = Console.ReadLine();
+            bool fourth = int.TryParse(fourthInput, out fourthNumber);
+
+            if(!first || !second || !third || !fourth)
+            {
+                Console.WriteLine("Please enter a valid number!");
+                return;
+            }
+
+
             int avgResult = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
             Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber}, {fourthNumber} is {avgResult}!");
         }
