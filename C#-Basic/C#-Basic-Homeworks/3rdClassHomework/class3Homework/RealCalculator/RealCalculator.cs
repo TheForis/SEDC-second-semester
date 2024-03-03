f531c0a0b83bb2f742687688239f0dfb093d9b3b
TheForis <boriskrstovski@gmail.com>
2024-03-03 19:30:00 +0100

    hw

---
+++ C#-Basic/C#-Basic-Homeworks/3rdClassHomework/class3Homework/RealCalculator/RealCalculator.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+namespace RealCalculator
+{
+    internal class RealCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double firstNumber;
+            double secondNumber;
+            string chosenOperator;
+            while (true)
+            {
+                Console.WriteLine("Внесете број");
+                string firstUserInput = Console.ReadLine();
+                if (double.TryParse(firstUserInput, out firstNumber))
+                {
+                    break; 
+                }
+                Console.WriteLine("↓ Внесовте невалиден број! Обидете се повторно ↓");
+                Console.WriteLine();
+            }
+
+            /*while (true)
+            {
+                Console.WriteLine("Внесете математички оператор ( + - / *");
+                chosenOperator = Console.ReadLine();
+                if (chosenOperator == "+" || chosenOperator == "-" || chosenOperator == "/" || chosenOperator == "*")
+                {
+                    break;
+                }
+                Console.WriteLine("Внесовте невалиден математички оператор! Обидете се повторно");
+            }*/
+
+            while (true)
+            {
+                Console.WriteLine("Внесете втор број");
+                string secondUserInput = Console.ReadLine();
+                if (double.TryParse(secondUserInput, out secondNumber))
+                {
+                    break;
+                }
+                Console.WriteLine("↓ Внесовте невалиден број! Обидете се повторно ↓");
+                Console.WriteLine();
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Внесете математички оператор ( '+' '-' '/' '*' )");
+                chosenOperator = Console.ReadLine();
+
+                switch (chosenOperator)
+                {
+                    case "-":
+                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                        break;
+                    case "+":
+                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                        break;
+                    default: Console.WriteLine("↓ Внесовте невалиден математички оператор! Обидете се повторно ↓");
+                             Console.WriteLine();
+                        continue;
+
+                }
+                break;
+
+            }
+
+
+
+        }
+    }
+}
---
+++ C#-Basic/C#-Basic-Homeworks/3rdClassHomework/class3Homework/printingNumbers/PrintingNumbers.cs
@@ -0,0 +1,55 @@
+﻿namespace printingNumbers
+{
+    internal class PrintingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number");
+                string firstUserInput = Console.ReadLine();
+                if(int.TryParse(firstUserInput, out firstNumber))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter valid number");
+            }
+
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Please enter a number");
+                string secondUserInput = Console.ReadLine();
+                if (int.TryParse(secondUserInput, out secondNumber))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter valid number");
+            }
+
+            for (int i = secondNumber; i > 0; i--)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
