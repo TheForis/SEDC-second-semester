5585d9b2babaf9ca22bfd9e18da8b20dfcd81270
TheForis <boriskrstovski@gmail.com>
2024-03-07 10:48:54 +0100

    11

---
+++ C#-Basic/C#-Basic-Homeworks/5thHomework/5thHomework/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection.Metadata;
+
+namespace _5thHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime userDate;
+
+            while (true) { 
+            Console.WriteLine("Please enter day of birth! DD.MM.YYYY");
+            string userInput = Console.ReadLine();
+
+                if(DateTime.TryParseExact(userInput, "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture, System.Globalization.DateTimeStyles.AssumeUniversal, out userDate))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter valid date DD.MM.YYYY");
+            }
+            int ageResult = AgeCalculator(userDate);
+            Console.WriteLine($"You are {ageResult} years old!");
+        }
+
+
+        /*static string AgeCalculator (DateTime dateOfBirth)
+        {
+            DateTime currentDate = DateTime.Today;
+            int result = currentDate.Year - dateOfBirth.Year;
+            if(currentDate.Month == dateOfBirth.Month && currentDate.Day == dateOfBirth.Day)
+            {
+                return $"You are {result} years old and today is your birthday!!";
+            }
+            else if(currentDate.Month == dateOfBirth.Month && currentDate.Day < dateOfBirth.Day)
+            {
+                result--;
+            }
+            else if(currentDate.Month < dateOfBirth.Month)
+            {
+                result--;
+            }
+
+
+            return $"You are {result} years old";
+        }
+
+
+        ^^^ Mene ova povekje mi se dopagja kako reshenie - ama neznam shto e podobro vo praksa pa ete poednostaven kod podole koj vrakja starost.
+*/
+
+        static int AgeCalculator(DateTime dateOfBirth)
+        {
+            DateTime currentDate = DateTime.Today;
+            int result = currentDate.Year - dateOfBirth.Year;
+            if (currentDate.Month == dateOfBirth.Month && currentDate.Day < dateOfBirth.Day)
+            {
+                result--;
+            }
+            else if (currentDate.Month < dateOfBirth.Month)
+            {
+                result--;
+            }
+            return result;
+        }
+    }
+}
---
+++ C#-Basic/C#-Basic-Homeworks/5thHomework/SubstringsApp/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace SubstringsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int userNumber;
+            while (true)
+            {
+                Console.Write("Please enter a number:");
+                string userInput = Console.ReadLine();
+                if(int.TryParse(userInput, out userNumber)){
+                    break;
+                }
+                Console.WriteLine("Please enter a valid number");
+            }
+
+            /*string methodResult = Substrings(userNumber);   
+            Console.WriteLine(methodResult);*/
+
+
+            Substrings(userNumber);
+            
+
+        }
+        /*static string Substrings (int number)
+        {
+            string message = "Hello from QINSHIFT Codecademy 2024";
+            if (number < 1) 
+            {
+                return "Please enter a positive number!";
+                
+            }
+            else if (number > message.Length) 
+            {
+                return "Please enter lower number";
+            }
+            string changedMessage = message.Substring(0,number);
+            Console.WriteLine($"The length of the new string is: {changedMessage.Length} characters.");
+            return changedMessage;*/
+
+
+
+        static void Substrings(int number)
+            {
+            string message = "Hello from QINSHIFT Codecademy 2024";
+            if (number < 1)
+            {
+                Console.WriteLine( "Please enter a positive number!");
+                return;
+
+            }
+            else if (number > message.Length)
+            {
+                Console.WriteLine("Please enter lower number");
+                return;
+            }
+            string changedMessage = message.Substring(0, number);
+            Console.WriteLine(changedMessage);
+            Console.WriteLine($"The length of the new string is: {changedMessage.Length} characters.");
+            }
+    }
+}
