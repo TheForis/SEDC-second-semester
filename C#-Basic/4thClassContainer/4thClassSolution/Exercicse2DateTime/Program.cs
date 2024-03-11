38477dcf402be83d4f074fed583731be8d69136c
TheForis <boriskrstovski@gmail.com>
2024-03-11 19:15:43 +0100

    11

---
+++ C#-Basic/4thClassContainer/4thClassSolution/Exercicse2DateTime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Exercicse2DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            DateTime now = DateTime.Now;
+
+            DateTime threeDaysFromNow = now.AddDays(3);
+            Console.WriteLine(threeDaysFromNow);
+
+            DateTime monthFromNow = now.AddMonths(1);
+            Console.WriteLine(monthFromNow);
+
+            DateTime monthThreeDaysFromNow = AddDaysAndMonthsFromNow(3,1);
+            Console.WriteLine(monthThreeDaysFromNow);
+
+            DateTime yearAnd2MonthsBefore = now.AddYears(-1).AddMonths(-2);
+            Console.WriteLine(yearAnd2MonthsBefore);
+
+            Console.WriteLine($"{now.ToString("MMMM")}");
+
+            Console.WriteLine(now.Month);
+
+            Console.WriteLine(now.Year);
+
+
+
+        }
+        // public - access modifier, defines from where this method can be accessed.
+        //static - because Main method(the Main brackets) is static
+        //DateTime - return Type, result that the method returns
+        // Name of the method
+        // (int, string, datetime ) - input parametars to the method, needs to be specified
+        public static DateTime AddDaysAndMonthsFromNow(int days, int months)
+        {
+            DateTime now = DateTime.Now;
+            
+            return now.AddDays(days).AddMonths(months);
+        }
+
+
+    }
+}
---
+++ C#-Basic/5thClassContainer/5thClassSolution/Demo1Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo1Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class - shablon za kreiranje na objekti. se pravat preku konstruktori
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow");
+        }
+    }
+}
