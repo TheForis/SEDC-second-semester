f13c2fa3df605bc199e1eeae6043451f030f4fc4
TheForis <boriskrstovski@gmail.com>
2024-03-13 15:00:48 +0100

    hw

---
+++ C#-Basic/C#-Basic-Homeworks/6thClassHoework/Task1/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace Task1
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public Color Color { get; set; }
+
+        public Dog() 
+        {
+        }
+        public Dog(string name, string breed, Color color) 
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+        public string Eat()
+        {
+            return $"The dog of breed: {Breed}, name: {Name} and {Color} is now eating!";
+        }
+        public string Play() 
+        {
+            return $"The dog of breed: {Breed}, name: {Name} and {Color} is now playing!";
+        }
+        public string ChaseTail()
+        {
+            return $"The dog of breed: {Breed}, name: {Name} and {Color} is now chasing it's tail!";
+        }
+        
+    }
+}
---
+++ C#-Basic/C#-Basic-Homeworks/6thClassHoework/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] messages = new string[]{
+                "Please enter the name of your Dog!",
+                "Please enter the breed of your Dog!",
+                "Please enter the color of your Dog!",
+                "Please enter 1(Eat), 2(Play) or 3(To chase the Tail)"
+                };
+
+            Dog userDog = new Dog(GetUserInput(messages[0]), GetUserInput(messages[1]), Color.FromName(GetUserInput(messages[2])));
+
+            while (true)
+            {
+                switch (GetUserInput(messages[3]))
+                    {
+                    case "1":
+                        Console.WriteLine(userDog.Eat());
+                        return;
+                    case "2":
+                        Console.WriteLine (userDog.Play());
+                        return;
+                    case "3":
+                        Console.WriteLine(userDog.ChaseTail());
+                        return;
+                     }
+
+            }
+
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+    }  
+}
