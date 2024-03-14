adee3d3ed8bcca841f19807829e2d6dfe78e2c40
TheForis <boriskrstovski@gmail.com>
2024-03-15 00:44:33 +0100

    hw

---
+++ C#-Basic/C#-Basic-Homeworks/7thClassHomework/Task/NumberStats.cs
@@ -0,0 +1,42 @@
+﻿namespace Task
+{
+    public class NumberStats
+    {
+        public decimal Number { get; set; }
+
+        public NumberStats (decimal number) 
+        {
+            Number = number;
+        }
+        public string IsPositive { get
+            {
+                if (Number >= 0)
+                {
+                    return "Positive";
+                }
+                return "Negative";
+            }
+        }
+        public string IsInteger { get
+            {
+                if(Number % 1 == 0)
+                {
+                    return "Integer";
+                }
+                return "Decimal";
+            } }
+        public string IsEven { get
+            {
+                if (Number % 2 == 0)
+                {
+                    return "Even";
+
+                }
+                return "Odd";
+            } }
+        public string GetNumberStats()
+        {
+            return $"Stats for number: {Number} \n * {IsPositive} \n * {IsInteger} \n * {IsEven}";
+        }
+    }   
+}
---
+++ C#-Basic/C#-Basic-Homeworks/7thClassHomework/Task/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                decimal userNumber = UserInputGetter();
+
+                NumberStats numberToCheck = new NumberStats(userNumber);
+                Console.WriteLine(numberToCheck.GetNumberStats());
+
+                if (!TryAgain())
+                {
+                    break;
+                }
+
+            }
+
+        }
+
+        static decimal UserInputGetter()
+        {
+            while (true)
+            {
+                Console.Write("Please enter a number -> ");
+                string userInput = Console.ReadLine();
+
+                if (decimal.TryParse(userInput, out decimal parsedNumber))
+                {
+                    return parsedNumber;
+                }
+                Console.WriteLine("Please enter a valid number!");
+            }
+        }
+
+        static bool TryAgain ()
+        {
+            while (true)
+            {
+                Console.Write("Do you want to try again? [Y / X] -> ");
+                string userAnswer = Console.ReadLine();
+                if (userAnswer == "Y" || userAnswer == "y")
+                {
+                    return true;
+                }
+                else if (userAnswer == "X" || userAnswer == "x")
+                {
+                    Console.WriteLine("Thank you for using our application!");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid answer! Y or X ");
+                }
+            }
+        }
+    }
+}
