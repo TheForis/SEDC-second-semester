7787bc459c86517360a676470e6432208c56d803
TheForis <149613209+TheForis@users.noreply.github.com>
2024-03-18 20:35:54 +0100

    Add files via upload

---
+++ C#-Basic/8thClassContainer/Demo4/Excercise/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> usersStack = new Stack<string>();
+
+            usersStack.Push("Boris");
+            usersStack.Push("Slave");
+            usersStack.Push("Sasho");
+
+            string peekUser =usersStack.Peek();
+
+            Console.WriteLine(peekUser);
+
+            string user1 = usersStack.Pop();
+            string user2 = usersStack.Pop();
+            bool success = usersStack.TryPop(out string user3);
+            bool success2 = usersStack.TryPop(out string user4);
+
+            List<string> list = new List<string>()
+            {
+                "niza","od","stringovi"
+            };
+
+            foreach (string item in list) { 
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine(user1);
+            Console.WriteLine(user2);
+            Console.WriteLine(user3);
+        }
+    }
+}
---
+++ C#-Basic/8thClassContainer/Demo4/excercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace excercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> phoneBook = new Dictionary<string, string>()
+            {
+                {"Boris", "078/-721-666" },
+                {"Sasho", "078/999-123"},
+                {"Aleksandra", "078/123-888" },
+                {"Maja", "078/766-444" },
+                {"Slave", "078/123-127" }
+
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a name to search ->");
+                string input = Console.ReadLine();
+
+                bool isItFound = phoneBook.TryGetValue(input, out string foundName);
+
+                if (!isItFound)
+                {
+                    Console.WriteLine("The name was not found in the phone book");
+                }
+                else
+                {
+                    Console.WriteLine($"We found the phone number for {input}. The phone number is: {foundName}");
+                }
+
+                Console.Write("Enter Y if you want to search again-> ");
+                string choice = Console.ReadLine();
+
+                if (!string.Equals(choice,"y", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
