8f2ba8a815eec11d59a324d0e6c00f289dc25b16
TheForis <boriskrstovski@gmail.com>
2024-03-20 02:06:11 +0100

    11

---
+++ C#-Basic/C#-Basic-Homeworks/9thClassHomework/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queueList = new Queue<int>(UserInput());
+
+            while (true)
+            {
+                if (queueList.TryDequeue(out int number))
+                {
+                    Console.WriteLine(number);
+                    continue;
+                }
+                break;
+            }
+            
+            
+        }
+
+        static Queue<int> UserInput()
+        {
+            Queue<int> integers = new Queue<int>();
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string userInput = Console.ReadLine();
+                if(int.TryParse(userInput,out int parsedInput))
+                {
+                    integers.Enqueue(parsedInput);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number!");
+                    continue;
+                }
+                Console.Write("Would you like to add another number? Y / N : ");
+                string userOption = Console.ReadLine();
+                if (userOption.ToLower() != "y")
+                {
+                    return integers;
+                }
+
+            }
+        }
+    }
+}
