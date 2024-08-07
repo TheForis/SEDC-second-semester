33cc1a1b98970d3cebf35000458180acbd06cd79
TheForis <boriskrstovski@gmail.com>
2024-08-07 17:53:14 +0200

    1
    1

---
+++ WebApi/1stClassContainer/Demo1/Demo1_OurFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1_OurFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
